c31f824222f6561f543bc23ffc376a308ce6cafd
Radenkovic1981 <radenkovic88@yahoo.com>
2022-04-13 15:56:52 +0200

    quick fixes

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,7 +1,7 @@
 function Head() {
     return (
         <head>
-            <meta charset="utf-8" />
+            <meta charSet="utf-8" />
             <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <meta name="theme-color" content="#000000" />

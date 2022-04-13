bb19779e130a8e19b4745036088a8dd523df351b
Michael <mpathe@bettercollective.com>
2022-04-13 11:52:32 +0200

    clean
    remove alert
    increase headshot volume

--- src/utils/keyBinding.js
+++ src/utils/keyBinding.js
@@ -5,9 +5,7 @@ const keyBinding = (isGameRuning, runGame) => {
         keysPressed[event.key] = true;
 
         if (keysPressed['Control'] && event.key === ' ') {
-            if (window.confirm("Antistress break game. Press escape to exit. Happy destroying.")) {
-                runGame(true);
-            }
+            runGame(true);
         }
     });
 

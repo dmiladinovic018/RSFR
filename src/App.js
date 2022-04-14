a5907b58e10a20d17fe098ae5497935f97332e69
Michael <mpathe@bettercollective.com>
2022-04-14 12:18:59 +0200

    fixes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import EasterEggs from "./components/EasterEggs";
 import './sass/App.scss';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Content from './components/Content';
 
-function App({ routeMap }) {
+function App({routeMap}) {
     EasterEggs();
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Content homepage="true" />} />
-        {routeMap.map(({route, id, type}) => {
-          return (
-            <Route key={id} path={route} element={<Content id={id} type={type} homepage="false" />} />
-          );
-        })}
-      </Routes>
-    </Router>
-  );
+
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Content homepage="true"/>}/>
+                {routeMap.routes.map(({route, id, type}) => {
+                    return (
+                        <Route key={id} path={route} element={<Content id={id} type={type} homepage="false"/>}/>
+                    );
+                })}
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;

d9074af82e7e7f52ab18583c825e5b9f8e50f534
mdjordjevic-bc <mdjordjevic@bettercollective.com>
2022-04-14 11:43:23 +0200

    import fetched CSS files in head

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,17 +1,32 @@
+import { useEffect, useState } from "react";
+
 function Head() {
+    // [TO DO] Get from context
+    const domain = 'http://bcwp.hltv.test';
+    const restAPI = `${domain}/wp-json/wp/v2`;
+    const pluginAPI = `${domain}/wp-json/rsfr-rendpoint/v1`;
+
+    const [cssFiles, setCssFiles] = useState({});
+
+    useEffect(() => {
+        fetch(`${pluginAPI}/css`)
+        .then(response => response.json())
+        .then(data => {
+            setCssFiles(JSON.parse(data.css));
+        });
+    }, []);
+
     return (
         <head>
             <meta charSet="utf-8" />
-            <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <meta name="theme-color" content="#000000" />
             <meta
                 name="description"
                 content="Web site created using create-react-app"
             />
-            <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
-            <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
             <title>React App</title>
+            {Object.values(cssFiles).filter(url => url).map((url, index) => <link key={index} rel="stylesheet" href={url} />)}
         </head>
     );
 }

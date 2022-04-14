ed59051b9f19975e7f19470da777ff874a8f60cb
dmiladinovic018 <dmiladinovic@bettercollective.com>
2022-04-14 16:05:42 +0200

    footer and wraping content with conatiner

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,11 +1,13 @@
 import Head from "./Head";
 import Body from "./Body";
+import Footer from "./Footer"
 
 function Content({ id, type, homepage }) {
     return (
         <>
             <Head />
             <Body id={id} type={type} homepage={homepage} />
+            <Footer />
         </>
     );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+
+function Footer() {
+
+
+
+
+    return (
+        <footer className='footer'>
+            <div className="container footer-container">
+                <div className="row">
+                    <div className="col-lg-8 col-xs-8">
+                        <img src="./images/18playEN.jpg" alt="18+ Play responsible"/>
+                    </div>
+                    <div className="col-lg-4 col-xs-4">
+                        <img src="./images/gambling-therapy.jpg" alt="Gambling Therapy"/>
+                    </div>
+                </div>
+            </div>
+
+        </footer>
+    )
+
+}
+
+export default Footer;
